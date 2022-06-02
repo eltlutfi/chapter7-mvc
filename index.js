7c13634fdc289a235330f397b8f05667098955b0
eltlutfi <lutfi200499@gmail.com>
2022-06-02 18:58:57 +0700

    create mvc

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const router = require("./router");
+
 const PORT = 3000;
+
 const app = express();
 
-app.listen(PORT,() => {
-    console.log(`Liatening on http://localhost:${PORT}`);
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
 });
